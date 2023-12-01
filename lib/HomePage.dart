84f8847663b4952e02dfc5bd90971cd8a92fc3a0
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-01 18:45:09 +0900

    Home

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,6 +5,36 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  MaterialApp(
+       title: '時間割アプリ',
+      home: TimetableScreen(),
+    );
+  }
+}
+
+
+class TimetableScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('私の時間割'),
+      ),
+      body: Timetable(),
+    );
+  }
+}
+
+class Timetable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(title: Text('月曜日')),
+        ListTile(title: Text('9:00 - 数学')),
+        ListTile(title: Text('10:00 - 英語')),
+        // 他の曜日や時間も同様に追加
+      ],
+    );
   }
 }
