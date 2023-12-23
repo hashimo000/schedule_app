a55a32d41c48dbb329f67ec36ac2a88c82dab9c2
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-24 04:53:40 +0900

    timetableview

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:timetable_view/timetable_view.dart';
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-       title: '時間割アプリ',
+    return MaterialApp(
+      title: '時間割アプリ',
       home: TimetableScreen(),
     );
   }
 }
-
 
 class TimetableScreen extends StatelessWidget {
   @override
@@ -26,15 +25,63 @@ class TimetableScreen extends StatelessWidget {
 }
 
 class Timetable extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ListTile(title: Text('月曜日')),
-        ListTile(title: Text('9:00 - 数学')),
-        ListTile(title: Text('10:00 - 英語')),
-        // 他の曜日や時間も同様に追加
-      ],
+Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Timetable View Demo'),
+      ),
+      body: TimetableView(
+        laneEventsList: _buildLaneEvents(),
+        onEventTap: onEventTapCallBack,
+        timetableStyle: TimetableStyle(),
+        onEmptySlotTap: onTimeSlotTappedCallBack,
+      ),
     );
   }
-}
+
+  List<LaneEvents> _buildLaneEvents() {
+    return [
+      LaneEvents(
+        lane: Lane(name: 'Track A', laneIndex: 1),
+        events: [
+          TableEvent(
+            title: 'An event 1',
+            eventId: 11,
+            startTime: TableEventTime(hour: 8, minute: 0),
+            endTime: TableEventTime(hour: 10, minute: 0),
+            laneIndex: 1,
+          ),
+          TableEvent(
+            eventId: 12,
+            title: 'An event 2',
+            laneIndex: 1,
+            startTime: TableEventTime(hour: 12, minute: 0),
+            endTime: TableEventTime(hour: 13, minute: 20),
+          ),
+        ],
+      ),
+      LaneEvents(
+        lane: Lane(name: 'Track B', laneIndex: 2),
+        events: [
+          TableEvent(
+            title: 'An event 3',
+            laneIndex: 2,
+            eventId: 21,
+            startTime: TableEventTime(hour: 10, minute: 10),
+            endTime: TableEventTime(hour: 11, minute: 45),
+          ),
+        ],
+      ),
+    ];
+  }
+
+  void onEventTapCallBack(TableEvent event) {
+    print(
+        "Event Clicked!! LaneIndex ${event.laneIndex} Title: ${event.title} StartHour: ${event.startTime.hour} EndHour: ${event.endTime.hour}");
+  }
+
+  void onTimeSlotTappedCallBack(
+      int laneIndex, TableEventTime start, TableEventTime end) {
+    print(
+        "Empty Slot Clicked !! LaneIndex: $laneIndex StartHour: ${start.hour} EndHour: ${end.hour}");
+  }}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:schedule/HomePage.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
