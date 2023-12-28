27320e68b1eb0f30b7d658199cf99977926ebede
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-28 11:06:34 +0900

    word change

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -29,7 +29,7 @@ class TimetableScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('時間割！'),
+        title: Text('時間割'),
         actions: [
           IconButton(
             icon: Icon(Icons.settings),
