936f207862a4bacea57f11b77b473191fc15949a
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-31 19:17:14 +0900

    Text Fieldを追加

--- lib/DetailScreen .dart
+++ lib/DetailScreen .dart
@@ -5,12 +5,33 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('授業の追加情報'),
       ),
-      body: const Center(
-        child: TextField(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Columnを中心に配置
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '授業名',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 8), // テキストフィールド間のスペース
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '教室',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              // 他にもTextFieldを追加可能
+            ],
+          ),
+        ),
       ),
     );
   }
