5952c9d01cd6368d86659b48cde4849c395ad9a3
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-31 12:13:25 +0900

    Text Field

--- lib/DetailScreen .dart
+++ lib/DetailScreen .dart
@@ -10,7 +10,7 @@ class DetailScreen extends StatelessWidget {
         title: const Text('授業の追加情報'),
       ),
       body: const Center(
-        child:Text('ここに授業の詳細情報を表示'),
+        child: TextField(),
       ),
     );
   }
