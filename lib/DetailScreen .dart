d895e529891fb17cfad5669d817fa18c05893014
hashimo000 <hashimon.hashimon@icloud.com>
2024-01-02 12:26:00 +0900

    欠席ボタン

--- lib/DetailScreen .dart
+++ lib/DetailScreen .dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
+
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +53,31 @@ class DetailScreen extends StatelessWidget {
                   border: OutlineInputBorder(),
                 ),
               ),
-              // 他にもTextFieldを追加可能
+              Text("欠席回数"),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: _incrementCounter,
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    onPressed: _decrementCounter,
+                    tooltip: 'Decrement',
+                    child: Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: _resetCounter,
+                    tooltip: 'Reset',
+                    child: Icon(Icons.refresh),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
