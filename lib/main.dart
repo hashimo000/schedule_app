aa1a8010604c1fc50ccc88abd86aed434a8df3be
hashimo000 <hashimon.hashimon@icloud.com>
2024-01-03 15:43:15 +0900

    ConsumerWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:schedule/HomePage.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(
+    child: MyApp()
+  ));
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
     return MaterialApp(
       theme: ThemeData(
  
