d2e5e9af6959ac0506aa79a6a76a50f84ab8a804
hashimo000 <hashimon.hashimon@icloud.com>
2024-01-03 22:58:15 +0900

    テキストの状態管理

--- lib/DetailScreen .dart
+++ lib/DetailScreen .dart
@@ -9,16 +9,32 @@ class CounterNotifier extends StateNotifier<int> {
   void reset() => state = 0;
 }
 
+// ClassNameの状態を管理するStateNotifier
+class ClassNameNotifier extends StateNotifier<String> {
+  // コンストラクタで初期状態を設定
+  ClassNameNotifier() : super('初期値');
+
+  // 状態を更新するメソッド
+  void updateText(String newText) {
+    state = newText; //状態を更新
+     }
+}
+
 // CounterNotifierを提供するProvider
 final counterProvider = StateNotifierProvider<CounterNotifier, int>((ref) {
   return CounterNotifier();
 });
-
+// ClassNameNotifierを提供するProvider
+final classnameProvider = StateNotifierProvider<ClassNameNotifier, String>((ref) {
+  return ClassNameNotifier();
+});
 // DetailScreenクラス内の変更点
 class DetailScreen extends ConsumerWidget {
+  final TextEditingController textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final counter = ref.watch(counterProvider);
+    final classname = ref.watch(classnameProvider); //状態を可視化
     return Scaffold(
       appBar: AppBar(
         title: const Text('授業の追加情報'),
@@ -30,12 +46,20 @@ class DetailScreen extends ConsumerWidget {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               TextField(
+                controller: textEditingController,//TextFieldの中身をtextEditingControllerで管理
                 decoration: InputDecoration(
                   border:OutlineInputBorder(),
                   labelText:"授業名"
                 ),
+                   
               ),
-             
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(classnameProvider.notifier).updateText(textEditingController.text);//ボタン押すとtextEditingControllerの中身をclassnameProviderで管理
+                  Navigator.pop(context);
+                },
+                child: const Text('保存'),
+              ),
               Text(
                 '欠席回数: $counter',
                 style: TextStyle(fontSize: 24),
@@ -46,17 +70,14 @@ class DetailScreen extends ConsumerWidget {
                 children: <Widget>[
                   FloatingActionButton(
                     onPressed: () => ref.read(counterProvider.notifier).increment(),
-                    tooltip: 'Increment',
                     child: Icon(Icons.add),
                   ),
                   FloatingActionButton(
                     onPressed: () => ref.read(counterProvider.notifier).decrement(),
-                    tooltip: 'Decrement',
                     child: Icon(Icons.remove),
                   ),
                   FloatingActionButton(
                     onPressed: () => ref.read(counterProvider.notifier).reset(),
-                    tooltip: 'Reset',
                     child: Icon(Icons.refresh),
                   ),
                 ],
