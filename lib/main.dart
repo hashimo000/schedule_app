3510c9b3373778e188ee81309f6b29cc0a2fac0c
hashimo000 <hashimon.hashimon@icloud.com>
2024-04-03 13:34:15 +0900

    ボトムナビゲーションバー

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,64 @@
 import 'package:schedule/HomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:schedule/inquiries.dart';
 void main() {
-  runApp(const ProviderScope(
+  runApp( ProviderScope(
     child: MyApp()
   ));
 }
+final bottomIndexProvider = StateProvider<int>((ref) => 0);
+class Root extends ConsumerWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // bottomIndexProviderを監視して、その値を取得します。
+    final bottomIndex = ref.watch(bottomIndexProvider);
+
+    // BottomNavigationBarItemのリストを定義します。
+    // ここでは「ホーム」と「その他」の2つのアイテムを作成しています。
+    final items = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'ホーム',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.question_answer),
+        label: 'その他',
+        
+      ),
+    ];
+
+    // BottomNavigationBarを定義します。
+    // `currentIndex`には`bottomIndex`の値を、`onTap`にはタップされた時の処理を定義しています。
+    final bar = BottomNavigationBar(
+      items: items,
+      currentIndex: bottomIndex,
+      backgroundColor: Color.fromARGB(255, 246, 108, 2), 
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white.withOpacity(0.5),
+      onTap: (index){
+        // bottomIndexProviderの値を更新します。
+        ref.read(bottomIndexProvider.notifier).state=index;
+      },
+    );
+    final pages =[
+      MyHomePage(),
+      InquiriesPage(),
+    ];
+
+    // Scaffoldウィジェットを使用して、BottomNavigationBarを画面下部に配置します。
+    // 通常、BottomNavigationBarはScaffoldのレイアウトの一部として使用されます。
+    return Scaffold(
+      body:pages[bottomIndex],
+      bottomNavigationBar: bar,
+    );
+  }
+}
+
 class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
-
-
+   MyApp({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {    
     return MaterialApp( 
@@ -19,7 +66,7 @@ class MyApp extends ConsumerWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const Root(),
       
     );
   }
